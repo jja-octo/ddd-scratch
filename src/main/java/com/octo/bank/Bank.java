05ef702996d0ac66c95f3bcafd44fa86d1a94dd4
Julien Jakubowski <jjakubowski@octo.com>
2019-10-31 17:32:31 +0100

    init

---
+++ src/main/java/com/octo/bank/Bank.java
@@ -0,0 +1,4 @@
+package com.octo.bank;
+
+public class Bank {
+}
---
+++ src/test/java/com/octo/bank/BankTest.java
@@ -0,0 +1,12 @@
+package com.octo.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BankTest {
+    @Test
+    public void test() {
+        assertThat(2).isEqualTo(2);
+    }
+}
