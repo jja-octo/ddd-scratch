05efdd63f61804743c78027704ec33f21e2dfa61
Julien Jakubowski <jjakubowski@octo.com>
2019-11-08 18:46:10 +0100

    DDD draft - simplifies step definitions & introduce a builder

--- src/main/java/com/octo/bank/ConversionRate.java
+++ src/main/java/com/octo/bank/ConversionRate.java
@@ -4,11 +4,18 @@ import java.math.BigDecimal;
 import java.util.Currency;
 
 public class ConversionRate {
-    private BigDecimal value;
+    private Currency sourceCurrency;
     private Currency targetCurrency;
+    private BigDecimal value;
 
-    public ConversionRate(BigDecimal value) {
+    public ConversionRate(Currency sourceCurrency, Currency targetCurrency, BigDecimal value) {
+        this.sourceCurrency = sourceCurrency;
+        this.targetCurrency = targetCurrency;
         this.value = value;
+    }
+
+    public static ConversionRateBuilder from(Currency currency) {
+        return new ConversionRateBuilder().addFromCurrency(currency);
     }
 
     public BigDecimal value() {
@@ -17,7 +24,7 @@ public class ConversionRate {
 
     public Money convert(Money currentMonney) {
         checkCurrencyCosntraints();
-        
+
         return new Money(applyTo(currentMonney.amount()), this.targetCurrency);
     }
 
@@ -27,5 +34,24 @@ public class ConversionRate {
 
     private BigDecimal applyTo(BigDecimal amount) {
         return value.multiply(amount);
+    }
+
+    public static class ConversionRateBuilder { // TODO use an immutable style or not?
+        private Currency fromCurrency;
+        private Currency toCurrency;
+
+        public ConversionRateBuilder addFromCurrency(Currency currency) {
+            this.fromCurrency = currency;
+            return this;
+        }
+
+        public ConversionRateBuilder to(Currency instance) {
+            this.toCurrency = fromCurrency;
+            return this;
+        }
+
+        public ConversionRate value(BigDecimal rate) {
+            return new ConversionRate(fromCurrency, toCurrency, rate);
+        }
     }
 }
--- src/test/java/com/octo/bank/MoneyConversionStepDefinition.java
+++ src/test/java/com/octo/bank/MoneyConversionStepDefinition.java
@@ -6,12 +6,20 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import java.math.BigDecimal;
+import java.util.Currency;
 
 public class MoneyConversionStepDefinition {
+
+    ConversionRate rate;
+
     @Given("^the \"([^\"]*)\" to \"([^\"]*)\" exchange rate is (.*)$")
     public void theToExchangeRateIs(String sourceCurrency, String targetCurrency, BigDecimal rate) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        // TODO implement a nice converter with cucumber so that we get a currency without this getInstance call
+        this.rate =
+                ConversionRate
+                        .from(Currency.getInstance(sourceCurrency))  
+                        .to(Currency.getInstance(targetCurrency))
+                        .value(rate);
     }
 
     @When("^the bank converts \"([^\"]*)\" to \"([^\"]*)\"$")
